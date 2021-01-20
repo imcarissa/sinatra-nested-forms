3cb04d605a1daf2ba359a2d23d8514dc90823cfb
imcarissa <carissa.sarreal@gmail.com>
2021-01-20 03:33:18 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@ships = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@ships << self
+    end
+
+    def self.all 
+        @@ships = []
+    end
 end
